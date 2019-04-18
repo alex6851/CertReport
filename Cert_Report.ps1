9bdb6565c12a7c8451fd8187626bde80fa6e568c
alexb <alexb@ad.mc.com>
2019-04-18 11:04:23 -0700

    Changed Subject line

--- Cert_Report.ps1
+++ Cert_Report.ps1
@@ -11,10 +11,10 @@ $SMTPServer = "10.160.60.57"
 $CAName = (Get-CA | select Computername).Computername
 
 #Get Details on Issued Certs
-$Output = Get-CA | Get-IssuedRequest -Filter "NotAfter -ge $(Get-Date)", "NotAfter -le $((Get-Date).AddMonths(4))" | select RequestID, CommonName, NotAfter, CertificateTemplate | sort Notafter
+$Output = Get-CA | Get-IssuedRequest -Filter "NotAfter -ge $(Get-Date)", "NotAfter -le $((Get-Date).AddMonths(6))" | select RequestID, CommonName, NotAfter, CertificateTemplate | sort Notafter
 
 #Take the above, and exclude CAExchange Certs, Select the first one, and get an integer value on how many days until the earliest renewal is necessary
-$RelevantInfo = ($Output | where-Object {$_.CertificateTemplate -notlike "CAExchange" -and $_.CertificateTemplate -notlike "1.3.6.1.4.1.311.21.8.1409591.2629332.1724303.11985699.9815899.150.16496907.14499982"})
+$RelevantInfo = ($Output | where-Object {$_.CertificateTemplate -notlike "CAExchange" -and $_.CertificateTemplate -notlike "1.3.6.1.4.1.311.21.8.1409591.2629332.1724303.11985699.9815899.150.16496907.14499982" -and $_.CertificateTemplate -notlike "DomainController"})
 $EarliestExpiryInteger = ([math]::abs(($Today - ($RelevantInfo[0].Notafter)).Days)).ToString()
 
 #Write the Relevant Info to a temp file
@@ -34,7 +34,13 @@ $PendingCountStr = $PendingCount.ToString()
 #Make the mail body
 $Body = "See Attached"
 
-$Subject = "PS Report - Issuing CA Info (Next Expiration is $EarliestExpiryInteger from now, $PendingCountStr Requests Pending)"
+if ($EarliestExpiryInteger -le 7)
+{
+    $Subject = "WARNING next Expiring Cert is $EarliestExpiryInteger from now, $PendingCountStr Requests Pending)"
+}
+else {
+    $Subject = "Expiring Certs Report nex Expiring Cert is $EarliestExpiryInteger from now, $PendingCountStr Requests Pending)"
+}
 
 Send-mailmessage -To $To -From $From -SmtpServer $SMTPServer -Subject $Subject -Body $Body -Attachments $TempFile
 
