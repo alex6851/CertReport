ea0c5f5e9f222174467f01410416e03b55224e54
alex6851 <44008749+alex6851@users.noreply.github.com>
2020-02-20 09:11:33 -0800

    Update Cert_Report.ps1

--- Cert_Report.ps1
+++ Cert_Report.ps1
@@ -6,9 +6,9 @@ Import-Module PSPKI
 #Variables
 $TempFile = "C:\Temp\CA_Report.html"
 $Today = get-date
-$To = "alexander.baker@mrcy.com"
-$From = "CERT_REPORT@mrcy.com"
-$SMTPServer = "mail.mrcy.com"
+$To = ""
+$From = ""
+$SMTPServer = ""
 
 #Get the CA Name
 $CAName = (Get-CA | select Computername).Computername
@@ -17,7 +17,7 @@ $CAName = (Get-CA | select Computername).Computername
 $Output = Get-CA | Get-IssuedRequest -Filter "NotAfter -ge $(Get-Date)", "NotAfter -le $((Get-Date).AddMonths(6))" | select RequestID, CommonName, NotAfter, CertificateTemplate | sort Notafter
 
 #Take the above, and exclude CAExchange Certs, Select the first one, and get an integer value on how many days until the earliest renewal is necessary
-$RelevantInfo = ($Output | where-Object {$_.CertificateTemplate -notlike "CAExchange" -and $_.CertificateTemplate -notlike "1.3.6.1.4.1.311.21.8.1409591.2629332.1724303.11985699.9815899.150.16496907.14499982" -and $_.CertificateTemplate -notlike "DomainController"})
+$RelevantInfo = ($Output | where-Object {$_.CertificateTemplate -notlike "CAExchange" -and $_.CertificateTemplate -notlike "DomainController"})
 $EarliestExpiryInteger = ([math]::abs(($Today - ($RelevantInfo[0].Notafter)).Days)).ToString()
 
 #Write the Relevant Info to a temp file
